6657fe3c389d401b632983e69e17e7eb2d5e986b
Stimmaster <stimmaster@gmail.com>
2023-02-09 16:04:14 +0300

    add Example002_a_Ravno_Xv2

---
+++ Example002_a_Ravno_Xv2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число");
+int i = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int j = Convert.ToInt32(Console.ReadLine());
+
+int sum = (int)Math.Pow(i, 2);
+if (sum == j) 
+{
+   Console.WriteLine("Да,  первое число является квадратом второго");
+}
+else 
+{
+Console.WriteLine("Нет,  первое число не является квадратом второго"); 
+}
